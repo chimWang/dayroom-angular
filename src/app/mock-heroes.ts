33795fe50a4926a1bad85fd08bfe1062ad3221ad
chimWang <chimwang1203@gmail.com>
2018-10-23 17:03:54 +0800

    chim

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: '曹操' },
+  { id: 2, name: '周瑜' },
+  { id: 3, name: '诸葛亮' },
+  { id: 4, name: '孙悟空' },
+  { id: 5, name: '虞姬' },
+  { id: 6, name: '武则天' },
+  { id: 7, name: '公孙策' },
+];
