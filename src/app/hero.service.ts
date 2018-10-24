c061194ac44f8f8775bd749c33e2297ac5b13aaa
chimWang <chimwang1203@gmail.com>
2018-10-24 18:53:17 +0800

    chim

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,19 +6,23 @@ import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
 
-  private heroesUrl = 'api/heroes'
-
-
+  private heroesUrl = 'api/heroes';
 
   constructor(
     private messageService: MessageService,
     private http: HttpClient
-  ) { }
+  ) {}
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
@@ -56,12 +60,30 @@ export class HeroService {
 
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`更新英雄的id为：${hero.id}`)),
       catchError(this.handleError<any>('更新英雄'))
     );
   }
+
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`添加英雄的id为：${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+
+  deleteHero(hero: Hero): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`删除了英雄${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+
 }
