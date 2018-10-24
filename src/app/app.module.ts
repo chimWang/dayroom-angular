a8410055ebb08edc2e1ac7e1e8ff24f2cadfeab5
chimWang <chimwang1203@gmail.com>
2018-10-24 17:34:55 +0800

    chim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { AppRoutingModule } from './app-routing.module'
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { MessageComponent } from './message/message.component'
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessageComponent } from './message/message.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+  ],
   declarations: [
+    MessageComponent,
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessageComponent,
-    DashboardComponent
   ],
-  imports: [
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule,
-    AppRoutingModule,
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,21 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: '曹操' },
+      { id: 2, name: '周瑜' },
+      { id: 3, name: '诸葛亮' },
+      { id: 4, name: '孙悟空' },
+      { id: 5, name: '虞姬' },
+      { id: 6, name: '武则天' },
+      { id: 7, name: '公孙策' },
+    ];
+    return { heroes };
+  }
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
