3d37b3887c00aeada3b2324a336cf7b59439bbf0
chimWang <chimwang1203@gmail.com>
2018-11-09 15:08:41 +0800

    chim

--- src/app/dayroom/bag/bag.component.ts
+++ src/app/dayroom/bag/bag.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bag',
   templateUrl: './bag.component.html',
-  styleUrls: ['./bag.component.css']
+  styleUrls: ['./bag.component.less']
 })
 export class BagComponent implements OnInit {
 
